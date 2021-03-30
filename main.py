a8e40c029e23167281abf6870251630466822fe1
Kirsch, Michael <michael.kirsch@stud.hs-kempten.de>
2021-03-30 10:52:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import can
+
+class CanCommunicator:
+    def __init__(self):
+        print("Can-Communication started")
+        pass
+
+class CoutCommunication:
+    def __init__(self):
+        print("Cout Communication started")
+        pass
+
+if __name__ == '__main__':
+    x = CoutCommunication()
